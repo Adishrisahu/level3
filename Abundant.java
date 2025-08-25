0a316d16d451acccabacccd507a033ea5ff3c94c
Adishrisahu <adishrisahu62@gmail.com>
2025-08-25 15:42:18 +0530

    Update Abundant.java

--- Abundant.java
+++ Abundant.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class Abundant{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -18,3 +19,4 @@ public class Abundant{
         sc.close();
     }
 }
+
